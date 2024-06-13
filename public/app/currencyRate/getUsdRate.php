ee1257885c8f1717338befc9acf1a59128fb8b84
Sultepronit <sultepronit@gmail.com>
2024-06-13 08:59:05 +0300

    cleaned

--- public/app/currencyRate/getUsdRate.php
+++ public/app/currencyRate/getUsdRate.php
@@ -3,14 +3,12 @@ declare(strict_types=1);
 function getUsdRate() {
     $ch = curl_init('https://minfin.com.ua/ua/currency/usd/');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $firstPageContent = curl_exec($ch);
+    $pageContent = curl_exec($ch);
     curl_close($ch);
-
-    // echo $firstPageContent;
 
     libxml_use_internal_errors(true);
     $html = new DOMDocument();
-    $html->loadHTML($firstPageContent);
+    $html->loadHTML($pageContent);
 
     $firstTbody = $html->getElementsByTagName('tbody')->item(0);
     // print_r($firstTbody);
@@ -20,12 +18,9 @@ function getUsdRate() {
     // print_r($firstP);
     $wholeText = $firstDiv->textContent;
     $redundantText = $firstP->textContent;
-    // echo $wholeText . '<br>';
-    // echo $redundantText . '<br>';
 
     $theRate = explode($redundantText, $wholeText)[0];
     $theRate = (float) str_replace(',', '.', $theRate);
 
-    // print_r(['rate' => round($theRate, 1)]);
     return ['rate' => round($theRate, 1)];
 }
