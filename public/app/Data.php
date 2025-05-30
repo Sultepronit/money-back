fc77fe4fca2570b37bda596438e1743f5ea260fc
sultepronit <sultepronit@gmail.com>
2025-05-30 19:17:56 +0000

    preparing ofr smart-update; improved several things

--- public/app/Data.php
+++ public/app/Data.php
@@ -34,7 +34,7 @@ class Data
         return $data;
     }
 
-    private static function getUpdatedDb(): ?array
+    private static function addLastDates(): bool
     {
         # check if last date is actual
         $query = 'SELECT date FROM main_table ORDER BY date DESC LIMIT 1';
@@ -42,18 +42,22 @@ class Data
 
         $dates = DateHandler::generateDateSpan($lastDate);
 
-        if(empty($dates)) return null;
+        if(empty($dates)) return false; // no new dates to add
 
         # insert passed dates to the db
-        $stmt = self::$pdo->prepare("INSERT INTO main_table ('date') VALUES (:value)");
+        // $stmt = self::$pdo->prepare("INSERT INTO main_table ('date') VALUES (:value)");
+        $query = "INSERT INTO main_table (`date`, v) VALUES (?, ?)";
+        $stmt = self::$pdo->prepare($query);
 
         foreach($dates as $date) {
-            $stmt->bindParam(':value', $date);
-            $stmt->execute();
+            // $stmt->bindParam(':value', $date);
+            // $stmt->execute();
+            $stmt->execute([$date, updateVersion(self::$pdo)]);
         }
 
         # return updated data
-        return self::getData();
+        // return self::getData();
+        return true; // just return true to indicate that the dates were added
     }
 
     private static function getWaitDebit() {
@@ -79,12 +83,15 @@ class Data
             return ['status' => 'success']; # congrats, you did id, don't try anymore!
         }
 
-        $updated = self::getUpdatedDb();
-        if($updated) {
-            return self::prepareForSending($updated);
-        } else {
-            return self::prepareForSending(self::getData());
-        }
+        // $updated = self::addLastDates();
+        // if($updated) {
+        //     return self::prepareForSending($updated);
+        // } else {
+        //     return self::prepareForSending(self::getData());
+        // }
+
+        self::addLastDates();
+        return self::prepareForSending(self::getData());
     }
 
     public static function refresh($pdo): ?array
@@ -99,15 +106,21 @@ class Data
             return ['status' => 'success']; # congrats, you did id, don't try anymore!
         }
 
-        $updated = self::getUpdatedDb();
-        if($updated) { # changed the date case
-            return self::prepareForSending($updated);
-        } else { # the date is actual case
-            if($receivedVersion === getDbVersion(self::$pdo)) {
-                return ['status' => 'up-to-date'];
-            } else {
-                return self::prepareForSending(self::getData());
-            }
+        // $updated = self::addLastDates();
+        // if($updated) { # changed the date case
+        //     return self::prepareForSending($updated);
+        // } else { # the date is actual case
+        //     if($receivedVersion === getDbVersion(self::$pdo)) {
+        //         return ['status' => 'up-to-date'];
+        //     } else {
+        //         return self::prepareForSending(self::getData());
+        //     }
+        // }
+
+        if(self::addLastDates() || $receivedVersion !== getDbVersion(self::$pdo)) {
+            return self::prepareForSending(self::getData());
         }
+
+        return ['status' => 'up-to-date'];
     }
 }
--- public/app/update.php
+++ public/app/update.php
@@ -16,21 +16,29 @@ function updateVersion(PDO $pdo): int
 
 function update(PDO $pdo, $date): array
 {
+    # get new version
+    $version = updateVersion($pdo);
+
     # receive the data
     [$column, $value, $passdata] = json_decode(file_get_contents('php://input'));
     // echo $passdata;
+    if(!checkPassdata($passdata, $pdo)) return ['status' => 'success'];
 
     # set data to db
-    $query = "UPDATE main_table SET {$column} = ? WHERE `date` = ?";
-    $pdo->prepare($query)->execute([$value, $date]);
+    $query = "UPDATE main_table SET {$column} = ?, v = ? WHERE `date` = ?";
+    $pdo->prepare($query)->execute([$value, $version, $date]);
 
     # fetch it back
-    $query = "SELECT {$column} FROM main_table WHERE `date` = '$date'";
-    $result = $pdo->query($query)->fetch(PDO::FETCH_COLUMN);
+    // $query = "SELECT {$column} FROM main_table WHERE `date` = '$date'";
+    // $result = $pdo->query($query)->fetch(PDO::FETCH_COLUMN);
+    $query = "SELECT {$column} FROM main_table WHERE `date` = ?";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([$date]);
+    $result = $stmt->fetch(PDO::FETCH_COLUMN);
 
     # send the results
     return (string) $result === (string) $value
-        ? ['version' => updateVersion($pdo)] : compact('value', 'result');
+        ? ['version' => $version] : compact('value', 'result');
 }
 
 function updateAddTable(PDO $pdo, $column): array
